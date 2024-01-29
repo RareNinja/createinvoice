6337ec58ffe41e8fb48adb6745bd59783d193f53
Davi Dalan <davi.balan@nelsonheusi.com>
2024-01-29 14:32:04 -0300

    Commit code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,293 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    exporter: {
+      name: "",
+      address: "",
+      zipCode: "",
+      phone: "",
+      cnpj: "",
+    },
+    importer: {
+      name: "",
+      address: "",
+      zipCode: "",
+      phone: "",
+      email: "",
+    },
+    paymentTerms: "",
+    countryOfShipment: "",
+    incoterms: "",
+    shippingDate: "",
+    countryOfOrigin: "",
+    sizeContainer: "",
+    items: [],
+  });
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    // Enviar dados do formulário para o servidor
+    console.log(data);
+  };
+
+  const handleAddItem = () => {
+    // Adicionar um novo campo de item ao formulário
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Invoice</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* Cabeçalho da Fatura */}
+        <section className="bg-gray-200 p-4 mb-4">
+          <h2>Exportador</h2>
+          <div className="grid grid-cols-2 gap-4">
+            <input
+              type="text"
+              name="exporter.name"
+              // ref={register({ required: true })}
+              placeholder="Nome"
+              className="border-gray-300 rounded-md p-2"
             />
-          </a>
+            <input
+              type="text"
+              name="exporter.address"
+              // ref={register({ required: true })}
+              placeholder="Endereço"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="exporter.zipCode"
+              // ref={register({ required: true })}
+              placeholder="CEP"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="exporter.phone"
+              // ref={register({ required: true })}
+              placeholder="Telefone"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="exporter.cnpj"
+              // ref={register({ required: true })}
+              placeholder="CNPJ"
+              className="border-gray-300 rounded-md p-2"
+            />
+          </div>
+
+          <h2>Importador</h2>
+          <div className="grid grid-cols-2 gap-4">
+            <input
+              type="text"
+              name="importer.name"
+              // ref={register({ required: true })}
+              placeholder="Nome"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="importer.address"
+              // ref={register({ required: true })}
+              placeholder="Endereço"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="importer.zipCode"
+              // ref={register({ required: true })}
+              placeholder="CEP"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="importer.phone"
+              // ref={register({ required: true })}
+              placeholder="Telefone"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="importer.Email"
+              // ref={register({ required: true })}
+              placeholder="Email"
+              className="border-gray-300 rounded-md p-2"
+            />
+          </div>
+
+          <h2>Detalhes da Fatura</h2>
+          <div className="grid grid-cols-2 gap-4">
+            <input
+              type="text"
+              name="paymentTerms"
+              // ref={register()}
+              placeholder="Termos de Pagamento"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="countryOfShipment"
+              // ref={register()}
+              placeholder="País de Embarque"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="incoterms"
+              // ref={register()}
+              placeholder="Incoterms"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="date"
+              name="shippingDate"
+              // ref={register()}
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="countryOfOrigin"
+              // ref={register()}
+              placeholder="País de Origem"
+              className="border-gray-300 rounded-md p-2"
+            />
+            <input
+              type="text"
+              name="sizeContainer"
+              // ref={register()}
+              placeholder="Tamanho do Container"
+              className="border-gray-300 rounded-md p-2"
+            />
+          </div>
+        </section>
+        <h2>Itens da Fatura</h2>
+        <div className="grid grid-cols-6 gap-4">
+          <div className="col-span-4">
+            <table className="table-auto w-full">
+              <thead>
+                <tr>
+                  <th className="border px-4 py-2">Quantidade</th>
+                  <th className="border px-4 py-2">Unit</th>
+                  <th className="border px-4 py-2">NCM/SH</th>
+                  <th className="border px-4 py-2">Descrição</th>
+                  <th className="border px-4 py-2">País de Fabricação</th>
+                  <th className="border px-4 py-2">Moeda</th>
+                  <th className="border px-4 py-2">Preço Unitário</th>
+                  <th className="border px-4 py-2">Preço Total</th>
+                  <th className="border px-4 py-2">Peso Líquido Total (Kg)</th>
+                  <th className="border px-4 py-2">Peso Bruto Total (Kg)</th>
+                </tr>
+              </thead>
+              <tbody>
+                {formData.items.map((item, index) => (
+                  <tr key={index}>
+                    <td className="border px-4 py-2">
+                      <input
+                        type="text"
+                        name={`formData.items[${index}].description`}
+                        value={item.description}
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            items: formData.items.map((i) =>
+                              i.id === item.id
+                                ? { ...i, description: e.target.value }
+                                : i
+                            ),
+                          })
+                        }
+                        className="border-gray-300 rounded-md p-2"
+                      />
+                    </td>
+
+                    <td className="border px-4 py-2">
+                      <input
+                        type="number"
+                        name={`formData.items[${index}].quantity`}
+                        value={String(item.quantity)} // Convertendo para string
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            items: formData.items.map((i) =>
+                              i.id === item.id
+                                ? { ...i, quantity: e.target.value }
+                                : i
+                            ),
+                          })
+                        }
+                        className="border-gray-300 rounded-md p-2"
+                      />
+                    </td>
+
+                    <td className="border px-4 py-2">
+                      <input
+                        type="number"
+                        name={`formData.items[${index}].unitPrice`}
+                        value={String(item.unitPrice)} // Convertendo para string
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            items: formData.items.map((i) =>
+                              i.id === item.id
+                                ? { ...i, unitPrice: e.target.value }
+                                : i
+                            ),
+                          })
+                        }
+                        className="border-gray-300 rounded-md p-2"
+                      />
+                    </td>
+                    <td className="border px-4 py-2">
+                      {/* Exibir o total de cada item */}
+                      {item.quantity * item.unitPrice}
+                    </td>
+
+                    <td className="border px-4 py-2">
+                      <button
+                        type="button"
+                        onClick={() => handleRemoveItem(index)}
+                        className="bg-red-500 text-white px-4 py-2 rounded-md"
+                      >
+                        Remover
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="col-span-2">
+            <button
+              type="button"
+              onClick={handleAddItem}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Adicionar Item
+            </button>
+          </div>
+
+          <h2>Total da Fatura</h2>
+          <div className="bg-gray-200 p-4">
+            {/* <p className="font-bold">Total: R$ {calculateTotal()}</p> */}
+          </div>
+
+          <button
+            type="submit"
+            className="bg-green-500 text-white px-4 py-2 rounded-md mt-4"
+          >
+            Enviar Fatura
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </form>
+    </div>
   );
 }
