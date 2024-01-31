eb30006e06f20b058d15bcf8f111133d4d74489b
Davi Dalan <davi.balan@nelsonheusi.com>
2024-01-31 17:21:01 -0300

    Commit criação de janela para adição dos itens

---
+++ src/app/components/JanelaItem.tsx
@@ -0,0 +1,201 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type InvoiceItem = {
+  quantity: string;
+  description: string;
+  unitPrice: string;
+};
+
+const [formData, setFormData] = useState({
+  exporter: {
+      name: "",
+      address: "",
+      zipCode: "",
+      phone: "",
+      cnpj: "",
+  },
+  importer: {
+      name: "",
+      address: "",
+      zipCode: "",
+      phone: "",
+      email: "",
+  },
+  paymentTerms: "",
+  countryOfShipment: "",
+  incoterms: "",
+  shippingDate: "",
+  countryOfOrigin: "",
+  sizeContainer: "",
+  items: [{} as InvoiceItem],
+});
+
+const handleAddItem = () => {
+  setFormData({
+    ...formData,
+    items: [
+      ...formData.items,
+      {
+        description: "",
+        quantity: "",
+        unitPrice: "",
+      },
+    ],
+  });
+};
+
+const handleRemoveItem = (index: any) => {
+  const updatedItems = [...formData.items];
+  updatedItems.splice(index, 1);
+
+  setFormData({
+    ...formData,
+    items: updatedItems,
+  });
+};
+const Windowsitem = ({}) => {
+  return (
+    <div className="grid grid-cols-2 gap-4 w-full overflow-x-auto">
+                        <div className="col-span-2">
+                            <table className="table-auto">
+                                <thead>
+                                    <tr className="border-2 px-4 py-2 text-black">
+                                        <th className="border-2">Quantidade</th>
+                                        <th className="border-2">Unit</th>
+                                        <th className="border-2">NCM/SH</th>
+                                        <th className="border-2">Descrição</th>
+                                        <th className="border-2">País de Fabricação</th>
+                                        <th className="border-2">Moeda</th>
+                                        <th className="border-2">Preço Unitário</th>
+                                        <th className="border-2">Preço Total</th>
+                                        <th className="border-2">Peso Líquido Total (Kg)</th>
+                                        <th className="border-2">Peso Bruto Total (Kg)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {formData.items.map((item, index) => (
+                                        <tr key={index}>
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="number"
+                                                    name={`formData.items[${index}].quantity`}
+                                                    value={item.quantity}
+                                                    onChange={(e) =>
+                                                        setFormData({
+                                                            ...formData,
+                                                            items: formData.items.map((i, idx) =>
+                                                                idx === index
+                                                                    ? { ...i, quantity: e.target.value }
+                                                                    : i
+                                                            ),
+                                                        })
+                                                    }
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="number"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="number"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="text"
+                                                    name={`formData.items[${index}].description`}
+                                                    value={item.description}
+                                                    onChange={(e) =>
+                                                        setFormData({
+                                                            ...formData,
+                                                            items: formData.items.map((i) =>
+                                                                i === item
+                                                                    ? { ...i, description: e.target.value }
+                                                                    : i
+                                                            ),
+                                                        })
+                                                    }
+                                                    className="border-gray-300 border rounded-md p-2"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="text"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="text"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="number"
+                                                    name="formData.items.unitPrice"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="number"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="number"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2 text-black">
+                                                <input
+                                                    type="number"
+                                                    className="border-gray-300 border rounded-md p-2 text-black"
+                                                />
+                                            </td>
+
+                                            <td className="border-2 px-4 py-2">
+                                                <button
+                                                    type="button"
+                                                    onClick={() => handleRemoveItem(index)}
+                                                    className="bg-red-500 text-white px-4 py-2 rounded-md"
+                                                >
+                                                    Remover
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div className="col-span-2">
+                      <button
+                        type="button"
+                        onClick={handleAddItem}
+                        className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                      >
+                        Adicionar Item
+                      </button>
+                    </div>
+  );
+};
+
+export default Windowsitem;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
+import Windowsitem from "./components/JanelaItem";
 export default function Home() {
     type InvoiceItem = {
         quantity: string;
@@ -42,34 +42,9 @@ export default function Home() {
         // Enviar dados do formulário para o servidor
         console.log(data);
     };
-
-    const handleAddItem = () => {
-        setFormData({
-            ...formData,
-            items: [
-                ...formData.items,
-                {
-                    description: "",
-                    quantity: "",
-                    unitPrice: "",
-                },
-            ],
-        });
-    };
-
     const handleUpdateFields = (field: any) => {
         setFields((prevValues: any) => ({ ...prevValues, [field.target.name]: field.target.value }))
     }
-
-    const handleRemoveItem = (index: any) => {
-        const updatedItems = [...formData.items];
-        updatedItems.splice(index, 1);
-
-        setFormData({
-            ...formData,
-            items: updatedItems,
-        });
-    };
 
     const changeColorInvoice = (className: string) => {
         setColorInvoice(className);
@@ -259,141 +234,11 @@ export default function Home() {
                       <div className="col-span-2">
                               <button
                                   type="button"
-                                  onClick={handleAddItem}
+                                  // onClick={handleAddItem}
                                   className="bg-blue-500 text-white px-4 py-2 rounded-md"
                                   >
                                   Adicionar Item
                               </button>
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-2 gap-4 w-full overflow-x-auto">
-                        <div className="col-span-2">
-                            <table className="table-auto">
-                                <thead>
-                                    <tr className="border-2 px-4 py-2 text-black">
-                                        <th className="border-2">Quantidade</th>
-                                        <th className="border-2">Unit</th>
-                                        <th className="border-2">NCM/SH</th>
-                                        <th className="border-2">Descrição</th>
-                                        <th className="border-2">País de Fabricação</th>
-                                        <th className="border-2">Moeda</th>
-                                        <th className="border-2">Preço Unitário</th>
-                                        <th className="border-2">Preço Total</th>
-                                        <th className="border-2">Peso Líquido Total (Kg)</th>
-                                        <th className="border-2">Peso Bruto Total (Kg)</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {formData.items.map((item, index) => (
-                                        <tr key={index}>
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="number"
-                                                    name={`formData.items[${index}].quantity`}
-                                                    value={item.quantity}
-                                                    onChange={(e) =>
-                                                        setFormData({
-                                                            ...formData,
-                                                            items: formData.items.map((i, idx) =>
-                                                                idx === index
-                                                                    ? { ...i, quantity: e.target.value }
-                                                                    : i
-                                                            ),
-                                                        })
-                                                    }
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="number"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="number"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="text"
-                                                    name={`formData.items[${index}].description`}
-                                                    value={item.description}
-                                                    onChange={(e) =>
-                                                        setFormData({
-                                                            ...formData,
-                                                            items: formData.items.map((i) =>
-                                                                i === item
-                                                                    ? { ...i, description: e.target.value }
-                                                                    : i
-                                                            ),
-                                                        })
-                                                    }
-                                                    className="border-gray-300 border rounded-md p-2"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="text"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="text"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="number"
-                                                    name="formData.items.unitPrice"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="number"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="number"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2 text-black">
-                                                <input
-                                                    type="number"
-                                                    className="border-gray-300 border rounded-md p-2 text-black"
-                                                />
-                                            </td>
-
-                                            <td className="border-2 px-4 py-2">
-                                                <button
-                                                    type="button"
-                                                    onClick={() => handleRemoveItem(index)}
-                                                    className="bg-red-500 text-white px-4 py-2 rounded-md"
-                                                >
-                                                    Remover
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
