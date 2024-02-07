50263c4deef1fb44976fef2db97272411b3b7d48
Davi Dalan <davi.balan@nelsonheusi.com>
2024-02-07 13:47:59 -0300

    commit code

---
+++ src/app/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+export default function LoadingComponent() {
+  return (
+    <Skeleton />
+  )
+} 
